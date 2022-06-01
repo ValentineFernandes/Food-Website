5404c23eb984b60534d2995b305551309ddd82c3
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-06-01 22:53:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+    let x = (window.innerWidth - e.pageX * 2) / 90;
+    let y = (window.innerHeight - e.pageY * 2) / 90;
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
+}
+
+document.querySelector('.home').onmouseleave = () =>{
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
+}   
